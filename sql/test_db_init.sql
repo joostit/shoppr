901d4b8bbe3667561ea4fa74a4731ede3a9125b7
Joost Haverkort <dev@joostit.com>
2022-07-18 15:45:21 +0200

    Changed testdb init sql script

--- sql/test_db_init.sql
+++ sql/test_db_init.sql
@@ -1,8 +1,8 @@
 
 -- First drop any existing database and user
-DROP DATABASE IF EXISTS db_shoppr;
-DROP USER IF EXISTS 'shoppruser'@'%';
+DROP DATABASE IF EXISTS db_shoppr_test; 
+DROP USER IF EXISTS 'shopprtestuser'@'%';
 
-create database db_shoppr; -- Creates the new database
-create user 'shoppruser'@'%' identified by 'ShopprUserDevPassword'; -- Creates the user
-grant all on db_shoppr.* to 'shoppruser'@'%'; -- Gives all privileges to the new user on the newly created database
+create database db_shoppr_test; -- Creates the new database
+create user 'shopprtestuser'@'%' identified by 'ShopprUserTestPassword'; -- Creates the user
+grant all on db_shoppr_test.* to 'shopprtestuser'@'%'; -- Gives all privileges to the new user on the newly created database
